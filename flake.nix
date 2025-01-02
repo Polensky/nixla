3aaa80b5d1eac3a0a041aa95d16ed38eb67aea9a
Polen <polensky@pm.me>
2025-01-01 22:04:38 -0500

    it work!

--- flake.nix
+++ flake.nix
@@ -1,30 +1,59 @@
 {
-  description = "A very basic flake";
+  description = "XLA miner";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    packages.default = 
-		let
-			version = "5.2.4";
-		in
-		nixpkgs.stdenv.mkDerivation {
-			name = "xlarig";
-			src = nixpkgs.fetchFromGitHub {
-				owner = "scala-network";
-				repo = "XLArig";
-				rev = "v${version}";
-				hash = "sha256-kFjUAOs92xExCV/teehee=";
-			};
+  outputs = { self, nixpkgs }: 
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          version = "5.2.4";
+        in
+        {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "xlarig";
+            inherit version;
+            
+            src = pkgs.fetchFromGitHub {
+              owner = "scala-network";
+              repo = "XLArig";
+              rev = "v${version}";
+              hash = "sha256-fMCuo0rp+Lve0gXkJEIfB0//g2VdlVOqieDvOBHFBYU=";
+            };
 
-			buildInputs = with nixpkgs; [
-				cmake
-				hwloc
-				openssl
-				libmicrohttpd
-			];
-		};
-  };
+            buildInputs = with pkgs; [
+              cmake
+              hwloc
+              openssl
+              libmicrohttpd
+              libuv
+            ];
+						
+						hardeningDisable = [ "fortify" ];
+
+            nativeBuildInputs = with pkgs; [
+              cmake
+            ];
+
+            configurePhase = ''
+              cmake -B build
+            '';
+
+            buildPhase = ''
+              cmake --build build --parallel $NIX_BUILD_CORES
+            '';
+
+            installPhase = ''
+              install -D build/xlarig $out/bin/xlarig
+            '';
+          };
+        });
+    };
 }
